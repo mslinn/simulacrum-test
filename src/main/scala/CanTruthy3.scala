f5870b435c4ecb75c83988cf4af595c3ac924328
Mike Slinn <mslinn@micronauticsresearch.com>
2019-09-29 09:56:31 -0700

    Looking forward to comments

--- src/main/scala/CanTruthy3.scala
+++ src/main/scala/CanTruthy3.scala
@@ -1,22 +1,23 @@
 import simulacrum._
 
-// FIXME Does not compile: value truthy is not a member of object North, South, East or West
-
 // True north is the only truthy direction
 sealed trait Direction
-object North extends Direction
-object South extends Direction
-object East  extends Direction
-object West  extends Direction
+class North extends Direction
+class South extends Direction
+class East  extends Direction
+class West  extends Direction
 
+/** Simulacrum's Ops pattern provides an implicit conversion from Direction to a generated class.
+ * (TODO need a sentence or two about this class... name? implicit? characteristics? members? docs?).
+ * This provides new methods for instances of Direction subtypes. */
 trait TruthyImplicits3 {
-  @typeclass trait Truthy[A <: Direction] { self =>
-    /** @return true if `a` is truthy. */
-    /*@op("truthy") */def truthy(a: A): Boolean
+  @typeclass trait Truthy[D <: Direction] {
+    /** @return true if `d` is truthy. */
+    /*@op("truthy") */def truthy(d: D): Boolean  // TODO why is @op not required? When is @op required?
   }
 
   implicit val directionCanTruthy: Truthy[Direction] = {
-    case North => true
+    case _: North => true
     case _ => false
   }
 }
@@ -24,12 +25,28 @@ trait TruthyImplicits3 {
 object CanTruthy3 extends App with TruthyImplicits3 {
   import Truthy.ops._
 
-  def north: Direction = North
-  def south: Direction = South
-  def east: Direction = East
-  def west: Direction = West
+  // TODO Verify that `D <: Direction : Truthy` means the following:
+  // Two bounds are applied to D:
+  //   1) A is constrained to be a subtype of Direction, or is of type Direction
+  //   2) A context bound parameter (implicit _: Truthy[+Direction]) is defined
+  def doTruthy1[D <: Direction : Truthy](d: D): Boolean = d.truthy
 
-  println(s"North.truthy = ${ north.truthy }")
+  // TODO this is simpler than doTruthy1, so it should be preferred. When is doTruthy1 a better choice?
+  def doTruthy2(d: Direction): Boolean = d.truthy
+
+  // If Cats is a dependency then we can write:
+  // import cats.Show
+  // def printTruthy[D : Truthy : Show](d: D): Unit = println(s"${ d.show }.truthy = ${ d.truthy }")
+  // TODO Again we see 2 constraints applied to A. What does `A : Truthy : Show` mean?
+
+  // Instances of Direction subtypes are implicitly enriched by the Truthy typeclass
+  val north: Direction = new North
+  val south: Direction = new South
+  val east: Direction = new East
+  val west: Direction = new West
+
+  println(s"North.truthy = ${ doTruthy1(north) }")
+  println(s"North.truthy = ${ doTruthy2(north) }")
   println(s"South.truthy = ${ south.truthy }")
   println(s"East.truthy  = ${ east.truthy }")
   println(s"West.truthy  = ${ west.truthy }")
